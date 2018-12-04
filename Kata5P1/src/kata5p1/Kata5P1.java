160e87d261275f0a6fda1f4ed3d86a4ecc6701d2
gonca <gonca@DESKTOP-UT10VI0>
2018-12-04 11:54:17 +0000

    Kata 5 Versión 1

--- Kata5P1/src/kata5p1/Kata5P1.java
+++ Kata5P1/src/kata5p1/Kata5P1.java
@@ -1,8 +1,12 @@
 package kata5p1;
 
+import SQLQueries.SelectApp;
+
 public class Kata5P1 {
 
     public static void main(String[] args) {
+        SelectApp sa = new SelectApp();
+        sa.selectAll("people");
     }
     
 }
